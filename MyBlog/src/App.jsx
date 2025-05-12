59bc0d82fb9074893fdbdca653e74b69170be54f
Akilex123 <aleksajeretin1@gmail.com>
2025-05-12 08:43:36 +0200

    Loading Screen

--- MyBlog/src/App.jsx
+++ MyBlog/src/App.jsx
@@ -8,45 +8,71 @@ import DashBoard from "./Pages/DashBoard";
 import Write from "./Pages/Write";
 import Read from "./Pages/Read";
 import IndPost from "./Pages/IndPost";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { ScaleLoader } from "react-spinners";
 
 function App() {
   const [username, setUsername] = useState("");
   const [post, setPost] = useState([]);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 15000);
+  }, []);
+
   const addPost = (newPost) => {
     setPost([...post, newPost]);
   };
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
-      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}></Navbar>
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/ourstory" element={<OurStory></OurStory>}></Route>
-        <Route
-          path="/login"
-          element={
-            <Login
-              setUsername={setUsername}
-              onLogin={() => setIsLoggedIn(true)}
-            ></Login>
-          } // postavljamo da login postavlja ime
-        ></Route>
-        <Route
-          path="/dashboard"
-          element={<DashBoard username={username}></DashBoard>} // postavljamo da dashboard preuzima ime
-        ></Route>
-        <Route
-          path="/write"
-          element={<Write addPost={addPost}></Write>}
-        ></Route>
-        <Route path="/read" element={<Read post={post}></Read>}></Route>
-        <Route
-          path="/read/:id"
-          element={<IndPost post={post}></IndPost>}
-        ></Route>
-      </Routes>
-    </div>
+    <>
+      {loading ? (
+        <div className="bg-black h-screen w-screen flex justify-center text-center">
+          <ScaleLoader
+            loading={loading}
+            color="white"
+            size={30}
+            className="m-auto"
+          />
+        </div>
+      ) : (
+        <div className="min-h-screen flex flex-col ">
+          <Navbar
+            isLoggedIn={isLoggedIn}
+            setIsLoggedIn={setIsLoggedIn}
+          ></Navbar>
+          <Routes>
+            <Route path="/" element={<Home></Home>}></Route>
+            <Route path="/ourstory" element={<OurStory></OurStory>}></Route>
+            <Route
+              path="/login"
+              element={
+                <Login
+                  setUsername={setUsername}
+                  onLogin={() => setIsLoggedIn(true)}
+                ></Login>
+              } // postavljamo da login postavlja ime
+            ></Route>
+            <Route
+              path="/dashboard"
+              element={<DashBoard username={username}></DashBoard>} // postavljamo da dashboard preuzima ime
+            ></Route>
+            <Route
+              path="/write"
+              element={<Write addPost={addPost}></Write>}
+            ></Route>
+            <Route path="/read" element={<Read post={post}></Read>}></Route>
+            <Route
+              path="/read/:id"
+              element={<IndPost post={post}></IndPost>}
+            ></Route>
+          </Routes>
+        </div>
+      )}{" "}
+    </>
   );
 }
 
